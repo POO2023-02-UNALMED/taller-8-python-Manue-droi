2acdafd621238f968e80e5e22efd770961d4dd5b
Manue-droi <manusarf16@gmail.com>
2023-10-16 16:00:30 -0500

    ya me canse xd

--- deportista.py
+++ deportista.py
@@ -1,19 +1,19 @@
 from persona import Persona
 
 class Deportista(Persona):
-    def __init__(self, nombre, edad, altura, sexo, deporte, AñosPracticando):
+    def __init__(self, nombre, edad, altura, sexo, deporte, años):
         super().__init__(nombre, edad, altura, sexo)
         self._deporte = deporte
-        self._AñosPracticando = AñosPracticando
-
-    #metodos get
-    def getDeporte(self):
-        return self._deporte
-    def getAñosPracticando(self):
-        return self._AñosPracticando
+        self._añosPracticando = años
+        
+    def setAñosPracticando(self, años):
+        self._añosPracticando = años
     
-    #metodos set
+    def getAñosPracticando(self):
+        return self._añosPracticando
+
     def setDeporte(self, deporte):
         self._deporte = deporte
-    def setAnosPracticados(self, AñosPracticando):
-       self._AñosPracticando = AñosPracticando
+
+    def getDeporte(self):
+        return self._deporte
