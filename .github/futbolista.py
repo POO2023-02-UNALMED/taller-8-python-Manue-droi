353ec5042f5dfde2c9796d164dbc8558a4ebb74f
Manue-droi <manusarf16@gmail.com>
2023-10-16 15:31:01 -0500

    sera q si?

--- .github/futbolista.py
+++ .github/futbolista.py
@@ -1,44 +1,49 @@
 from deportista import Deportista
 
 class Futbolista(Deportista):
-    _listaFutbolistas = []
-    def __init__(self,nombre, edad, altura, sexo, anosPracticados, golesMarcados, tarjetasRojas, piernaHabil):
-        super().__init__(nombre, edad, altura, sexo, "Futbol", anosPracticados)
-        self._golesMarcados = golesMarcados
-        self._tarjetasRojas = tarjetasRojas
+    
+    _listaFutbolistas= []
+    
+    def __init__(self, nombre, edad, altura, sexo, años, goles, tarjetas, piernaHabil):
+        super().__init__(nombre, edad, altura, sexo, "Futbol", años)
+        self._tarjetasRojas = tarjetas
+        self._golesMarcados = goles        
         self._piernaHabil = piernaHabil
         Futbolista._listaFutbolistas.append(self)
+
+    def setTarjetasRojas(self, tarjetas):
+        self._tarjetasRojas = tarjetas
     
-    #Meetodos get
-    def getGolesMarcados(self):
-        return self._golesMarcados
     def getTarjetasRojas(self):
         return self._tarjetasRojas
+    
+    def setGolesMarcados(self, goles):
+        self._golesMarcados = goles
+    
+    def getGolesMarcados(self):
+        return self._golesMarcados    
+    
+    def setPiernaHabil(self, pierna):
+        self._piernaHabil = pierna
+    
     def getPiernaHabil(self):
         return self._piernaHabil
     
-    #Meetodos set
-    def setGolesMarcados(self, golesMarcados):
-        self._golesMarcados = golesMarcados
-    def setTarjetasRojas(self, tarjetasRojas):
-        self._tarjetasRojas = tarjetasRojas
-    def setPiernaHabil(self, piernaHabil):
-        self._piernaHabil = piernaHabil
-
-    def __str__(self) :
-       cadena = "Mi nombre es {} soy profesional en el deporte {} Tengo {} años de edad y llevo {} años en el deporte".format(self.getNombre(), self.getDeporte(), str(self.getEdad()), str(self.getAnosPracticados()))
-       return cadena
+    def __str__(self):
+        cadena = "Mi nombre es {} soy profesional en el deporte {} Tengo {} años de edad y llevo {} años en el deporte".format(self.getNombre(), self.getDeporte(), str(self.getEdad()), str(self.getAñosPracticando()))
+        return cadena
 
 
     @classmethod
     def setListaFutbolistas(cls, futbolistas):
         cls._listaFutbolistas = futbolistas
-     
+    
     @classmethod
     def getListaFutbolistas(cls):
         return cls._listaFutbolistas
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
      futbolista = Futbolista("Felipe Perez", 21, "1,56", "M", 8, 189, 7, "Izquierda")
      print(futbolista.__str__())
-    
+
+  
