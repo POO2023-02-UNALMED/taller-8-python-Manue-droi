c4174d8d6a96844a907525d206e99f717ca0dad9
Manue-droi <manusarf16@gmail.com>
2023-10-16 14:59:14 -0500

    ggjhh

--- .github/futbolista.py
+++ .github/futbolista.py
@@ -5,7 +5,7 @@ class Futbolista(Deportista):
     def __init__(self,nombre, edad, altura, sexo, anosPracticados, golesMarcados, tarjetasRojas, piernaHabil):
         super.__init__(nombre, edad, altura, sexo, "Futbol", anosPracticados)
         self._golesMarcados = golesMarcados
-        self.tarjetasRojas = tarjetasRojas
+        self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
         Futbolista._listaFutbolistas.append(self)
     
@@ -17,4 +17,10 @@ class Futbolista(Deportista):
     def getPiernaHabil(self):
         return self._piernaHabil
     
-    
+    #Meetodos set
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
